4fe57a60a3f1a197acc43a4ed5b2feb117806c05
samuelmp58 <samuel.mauriliopereira@gmail.com>
2026-04-14 13:17:31 -0300

    corrigido

--- lista1/1-achei/main.cpp
+++ lista1/1-achei/main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
+#include <string>
 
 #define QNT 10
 
 int main() {
-  std::string nomes[10];
+    std::string nomes[QNT];
 
-  for (auto i{0}; i < QNT; ++i) {
-    std::cout << "Nome " << i + 1 << ": ";
-    std::cin >> nomes[i];
-  }
-
-  auto procurarPessoa = [&nomes](std::string target) -> void {
-    for (auto i{0}; i < QNT; ++i) {
-      if (nomes[i] == target)
-        std::cout << "ACHEI\n";
+    for (int i = 0; i < QNT; ++i) {
+        std::cout << "Nome " << i + 1 << ": ";
+        std::cin >> nomes[i];
     }
-  };
 
-  std::string pessoa;
-  std::cout << "Digite o nome de uma pessoa para ver se existe no array: ";
-  std::cin >> pessoa;
+    auto procurarPessoa = [&nomes](std::string target) -> void {
+        bool encontrado = false;
+        
+        for (int i = 0; i < QNT; ++i) {
+            if (nomes[i] == target) {
+                encontrado = true;
+                break;
+            }
+        }
 
-  procurarPessoa(pessoa);
+        if (encontrado) {
+            std::cout << "ACHEI\n";
+        } else {
+            std::cout << "NÃO ACHEI\n";
+        }
+    };
+
+    std::string pessoa;
+    std::cout << "\nDigite o nome de uma pessoa para ver se existe no array: ";
+    std::cin >> pessoa;
+
+    procurarPessoa(pessoa);
+
+    return 0;
 }
